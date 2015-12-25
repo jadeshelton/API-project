b167338df02fb02724528c341c2d2090b0d3bd3e
jadeshelton <jnshelton257@yahoo.com>
2015-12-24 22:04:06 -0800

    creating the styles

--- js/app.js
+++ js/app.js
@@ -14,29 +14,24 @@ function getRequest(searchTerm){
         $.ajax({
           url: "http://api.giantbomb.com/search/",
           type: "GET",
-          data: {api_key : "5afdc9b4e5bc62e549e77349e86e49c88dc07605", query: searchTerm, field_list : "name, image, platforms", format : "jsonp", json_callback : "gamer" },
+          data: {resource_type: "game", api_key : "5afdc9b4e5bc62e549e77349e86e49c88dc07605", query: searchTerm, field_list : ["id", "name", "genres"] ,  format : "jsonp", json_callback : "gamer" },
           dataType: "JSONP"
         });
-}
 
+
+}
+//trying to figure out how to pull genre data in, sketch out website first!
 function gamer(data) {
 	var searchTerm = $("#query").val();
-	$('body').append('<h1>There are ' + data.number_of_total_results + ' results for ' + searchTerm + '</h1>')
-	$.each(data.results, function(index, data) {
-    $('body').append('<p>' + data.name + ' ' + data.resource_type + '</p>');
-});
+	$('body').append('<h1>There are ' + data.number_of_total_results +' results for ' + searchTerm + '</h1');
+	$.each(data.results, function (index, item) {
+	$('body').append('<ul><li>' + item.name +  ': ' + '<a href="' + item.site_detail_url + '">' + item.site_detail_url + '</a>' + ' Genre: ' + item.genres + ' ' + item.image + '</li></ul>')
+})
 
-    // var games = data.game;
-//this works console.table(data.results, ['name','platforms']);
-  /*  $.each(data.results, function(index, value) {
-    	console.table(index + value)
-    });*/
-//this works	 console.table(data.results, ['name','platforms']);
-	// var result = $.grep(data.results, function(e){ return e.name; });
-//	 $('body').append('<h1>' +   data.results + '</h1>');
-       // alert( $( "body" ).data( data.results ) ); // undefined
-$.each( data.results, function( index, value ) {
-  console.log( value );
+$.each( data.results,  function( index, item ) {
+  console.log( item);
 });
 
 }
+
+// how to have var searchTerm = $("#query").val(); only one time in entire query?
