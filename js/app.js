88288f5897390b9d0798a3431ff6dcc094929875
jadeshelton <jnshelton257@yahoo.com>
2015-12-26 21:47:38 -0800

    working on API

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(function(){
 
 	$("#search-term").submit(function(event) {
 		event.preventDefault();
-		//$('#search-results').html('');
+	//	$('ul').html('');
 		var searchTerm = $("#query").val();
 		getRequest(searchTerm);
 		//$('nav').html('<h2 class="results-title">Results</h2>');
@@ -13,9 +13,9 @@ $(function(){
 
 function getRequest(searchTerm){
         $.ajax({
-          url: "http://api.giantbomb.com/api/search/",
+          url: "http://api.giantbomb.com/search/",
           type: "GET",
-          data: {limit: 4, resources: "game", api_key : "5afdc9b4e5bc62e549e77349e86e49c88dc07605", query: searchTerm, field_list : ["id", "name", "genres", "description", "platforms"] ,  format : "jsonp", json_callback : "gamer" },
+          data: {limit: 10, resource_type: "genre, game", api_key : "5afdc9b4e5bc62e549e77349e86e49c88dc07605", query: searchTerm, field_list : ["genres", "name", "platforms", "description"],  format : "jsonp", json_callback : "gamer" },
           dataType: "JSONP"
           
         });
@@ -31,8 +31,10 @@ function gamer(data) {
 	$.each(data.results, function (index, item) {
 	
 	//$('ul').append('dddddd')
-	$('ul').append('<li><div id="vg-name-and-desc"><div id="game-name"><h4>' + item.name + '</h4></div><div class="img-desc"><div id="game-image"><img src="' + item.image + '" alt="Quiz-app" title="Quiz App"></div><div id="game-desc"><p>' + item.description + '</p></div></div></div><div id="vg-rank-and-info"><div class="information" id="game-genre"><h3>Genre: ' + item.genre + '</h3></div><div class="information" id="game-rank"><h3>Ranking: ' + item.rank + '</h3></div><div class="information" id="game-platforms"><h3>Platforms: ' + item.platforms + '</h3></div><div class="information" id="game-link"><h3>Link: <a href="' + item.link + '">Link to game data</a></h3></div></div></li>')	
+	$('ul').append('<li><div id="vg-name-and-desc"><div id="game-name"><h4>' + item.name + '</h4></div><div class="img-desc"><div id="game-image"><img src="' + item.image.icon_url + '" alt="Quiz-app" title="Quiz App"></div><div id="game-desc"><p>' + item.description + '</p></div></div></div><div id="vg-rank-and-info"><div class="information" id="game-genre"><h3>Release Date: ' + item.original_release_date + '</h3></div><div class="information" id="game-rank"><h3>Ranking: ' + item.original_game_rating[0].name + '</h3></div><div class="information" id="game-platforms"><h3>Platforms: ' + item.platforms[0].name + '</h3></div><div class="information" id="game-link"><h3>Link: <a href="' + item.site_detail_url + '">Link to game data</a></h3></div></div></li>')	
 	//$('body').append('<ul><li>' + item.name +  ': ' + '<a href="' + item.site_detail_url + '">' + item.site_detail_url + '</a>' + ' Genre: ' + item.genres + ' ' + item.image + '</li></ul>')
+
+
 })
 
 $.each( data.results,  function( index, item ) {
